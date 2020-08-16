8a9dc438fedbc846a3cac0abebc61a90035ec77b
Giuliano Forghieri <gi.forghieri@gmail.com>
2020-08-16 21:59:40 +0200

    solve isogram

--- isogram/src/main/java/IsogramChecker.java
+++ isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,14 @@
+import java.lang.reflect.Array;
+
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (phrase.equals("")) return true;
+        for (int i = 0; i < phrase.length(); i++) {
+            int finalI = i;
+            long count = phrase.chars().map(Character::toLowerCase).filter(ch -> ch == phrase.charAt(finalI) && ch != '-' && ch != ' ').count();
+            if (count >= 2 ) return false;
+        }
+        return true;
     }
-
 }
